39ced50ce95e4703087476f1c01109b79008c8cf
Omri Nir <omrinir12@gmail.com>
2025-05-16 11:57:42 +0300

    Fix routes

--- server/controllers/playlistsRoutes.ts
+++ server/controllers/playlistsRoutes.ts
@@ -2,15 +2,15 @@ export async function playlistsRoutes(request: Request): Promise<Response> {
     const { method } = request;
     const { pathname } = new URL(request.url);
 
-    if (method === "GET" && pathname.startsWith("/playlists/")) {
-        const playlistId = pathname.split("/")[2];
+    if (method === "GET" && pathname.startsWith("/api/playlists/")) {
+        const playlistId = pathname.split("/")[3];
         return new Response(`Get Playlist by ID: ${playlistId}`, { status: 200 });
     } else if (method === "POST" && pathname === "/playlists") {
         return new Response("Create Playlist", { status: 201 });
-    } else if (method === "PUT" && pathname.startsWith("/playlists/")) {
-        const playlistId = pathname.split("/")[2];
+    } else if (method === "PUT" && pathname.startsWith("/api/playlists/")) {
+        const playlistId = pathname.split("/")[3];
         return new Response(`Update Playlist by ID: ${playlistId}`, { status: 200 });
-    } else if (method === "DELETE" && pathname.startsWith("/playlists/")) {
+    } else if (method === "DELETE" && pathname.startsWith("/api/playlists/")) {
         const playlistId = pathname.split("/")[2];
         return new Response(`Delete Playlist by ID: ${playlistId}`, { status: 200 });
     }

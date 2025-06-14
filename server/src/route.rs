9720190ceca140ece4214ff37f00ce64b0076423
OmriNR <omrinir12@gmail.com>
2025-06-14 19:47:28 +0300

    Creating songs service

--- server/src/route.rs
+++ server/src/route.rs
@@ -6,26 +6,24 @@ use axum::{
 
 use crate::{
     handler::{
-        create_todo_handler, delete_todo_handler, edit_todo_handler, get_todo_handler,
+        create_song_handler, get_song_handler,
         health_check_handler,
     },
     model,
 };
 
 pub fn create_router() -> Router {
-    let db = model::todo_db();
+    let db = model::song_db();
 
     Router::new()
         .route("/api/healthchecker", get(health_check_handler))
         .route(
                     "/api/todos",
-                    post(create_todo_handler),
+                    post(create_song_handler),
                 )
         .route(
-            "/api/todos/:id",
-            get(get_todo_handler)
-                .patch(edit_todo_handler)
-                .delete(delete_todo_handler),
+            "/api/todos/{id}",
+            get(get_song_handler)
         )
         .with_state(db)
 }

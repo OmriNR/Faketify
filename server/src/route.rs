dc49fac4291a4b352b0f9fad552c2317b06a03c9
OmriNR <omrinir12@gmail.com>
2025-06-15 13:21:45 +0300

    Adding the playlists methods

--- server/src/route.rs
+++ server/src/route.rs
@@ -7,23 +7,23 @@ use axum::{
 use crate::{
     handler::{
         create_song_handler, get_song_handler,
+        get_playlist_handler, delete_playlist_handler, create_playlist_handler, edit_playlist_handler,
         health_check_handler,
     },
     model,
 };
 
 pub fn create_router() -> Router {
-    let db = model::song_db();
+    let songsDB = model::song_db();
+    let playlistsDB = model::playlist_db();
 
     Router::new()
         .route("/api/healthchecker", get(health_check_handler))
-        .route(
-                    "/api/songs",
-                    post(create_song_handler),
-                )
-        .route(
-            "/api/songs/{id}",
-            get(get_song_handler)
-        )
-        .with_state(db)
+        .route("/api/songs", post(create_song_handler))
+        .route("/api/songs/{id}", get(get_song_handler))
+        .with_state(songsDB)
+        
+        .route("/api/playlists", post(create_playlist_handler))
+        .route("/api/playlists/{id}", get(get_playlist_handler).put(edit_playlist_handler).delete(delete_playlist_handler))
+        .with_state(playlistsDB)
 }

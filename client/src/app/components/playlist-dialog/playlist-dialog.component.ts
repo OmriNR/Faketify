51069cee02ac7fb67e361970f91011ba038096ec
Omri Nir <omrinir12@gmail.com>
2025-06-26 17:55:15 +0300

    Add drag-and-drop functionality to playlist creation dialog

--- client/src/app/components/playlist-dialog/playlist-dialog.component.ts
+++ client/src/app/components/playlist-dialog/playlist-dialog.component.ts
@@ -5,28 +5,62 @@ import { MatFormFieldModule} from "@angular/material/form-field";
 import { MatInputModule} from "@angular/material/input";
 import { MatButtonModule} from "@angular/material/button";
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-playlist-dialog',
-  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule, MatDialogActions, MatDialogContent],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule, MatDialogActions, MatDialogContent, MatIcon],
   templateUrl: './playlist-dialog.component.html',
   styleUrl: './playlist-dialog.component.scss'
 })
 export class PlaylistDialogComponent {
   playlistForm: FormGroup;
+  uploadedFiles: File[] = [];
 
   constructor(private dialogRef: MatDialogRef<PlaylistDialogComponent>, private fb: FormBuilder) {
     this.playlistForm = this.fb.group({
       name: ['', Validators.required],
-      songs: []
     });
+  }
+
+  onDragOver(event: DragEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    const dropZone = event.target as HTMLElement;
+    dropZone.classList.add('drag-over');
+  }
+
+  onDragLeave(event: DragEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    const dropZone = event.target as HTMLElement;
+    dropZone.classList.remove('drag-over');
+  }
+
+  onDrop(event: DragEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    const dropZone = event.target as HTMLElement;
+    dropZone.classList.remove('drag-over');
+
+    const files = Array.from(event.dataTransfer?.files || []);
+
+    if (files.every(file => file.type === 'audio/mp3')) {
+      if (files.length > 0) {
+        this.uploadedFiles = [...this.uploadedFiles, ...files];
+      }
+    }
+    else
+      alert('Only mp3 files are allowed');
   }
 
   onSubmit(): void {
     if (this.playlistForm.valid) {
-      console.log(this.playlistForm.value);
+      this.dialogRef.close({
+        name: this.playlistForm.value.name,
+        files: this.uploadedFiles
+      });
     }
-    this.dialogRef.close();
   }
 
   onCancel(): void {

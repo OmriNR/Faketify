d199e078331c59cda3e9d49735ad989c84b83b45
Omri Nir <omrinir12@gmail.com>
2025-07-13 07:09:06 +0300

    Adding search bar to the search component

--- client/src/app/components/search/search.component.ts
+++ client/src/app/components/search/search.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-
+import { FormsModule} from "@angular/forms";
+import { MatFormFieldModule} from "@angular/material/form-field";
+import { MatInputModule} from "@angular/material/input";
+import { MatIconModule} from "@angular/material/icon";
+import { MatButtonModule} from "@angular/material/button";
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [FormsModule, MatInputModule, MatFormFieldModule, MatIconModule, MatButtonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
+  searchValue: string = '';
+
+  clearSearch() {
+    this.searchValue = '';
+  }
 
 }

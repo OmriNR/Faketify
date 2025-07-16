846d9c0508b2f9abdf6a7e252885427385d9d818
Omri Nir <omrinir12@gmail.com>
2025-07-16 14:38:28 +0300

    Creating library component

--- client/src/app/components/library/library.component.ts
+++ client/src/app/components/library/library.component.ts
@@ -1,11 +1,121 @@
 import { Component } from '@angular/core';
+import {MatButton } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-library',
-  imports: [],
+  imports: [MatButton, MatCardModule, CommonModule],
   templateUrl: './library.component.html',
   styleUrl: './library.component.scss'
 })
 export class LibraryComponent {
+  selectedButton: string = '';
 
+  selectButton(button: string) {
+    this.selectedButton = button;
+  }
+
+  artists: any[] = [
+    {
+      name: 'John Doe',
+      songs: 50,
+      plays: 15000,
+    },
+    {
+      name: 'John Doe',
+      songs: 50,
+      plays: 15000,
+    },
+    {
+      name: 'John Doe',
+      songs: 50,
+      plays: 15000,
+    },
+    {
+      name: 'John Doe',
+      songs: 50,
+      plays: 15000,
+    },
+    {
+      name: 'John Doe',
+      songs: 50,
+      plays: 15000,
+    },
+    {
+      name: 'John Doe',
+      songs: 50,
+      plays: 15000,
+    },
+    {
+      name: 'John Doe',
+      songs: 50,
+      plays: 15000,
+    },
+    {
+      name: 'John Doe',
+      songs: 50,
+      plays: 15000,
+    }
+  ];
+  albums: any[] = [
+    {
+      name: 'Chill Vibes',
+      createdBy: 'John Doe'
+    },
+    {
+      name: 'Chill Vibes',
+      createdBy: 'John Doe'
+    },
+    {
+      name: 'Chill Vibes',
+      createdBy: 'John Doe'
+    },
+    {
+      name: 'Chill Vibes',
+      createdBy: 'John Doe'
+    },
+    {
+      name: 'Chill Vibes',
+      createdBy: 'John Doe'
+    },
+    {
+      name: 'Chill Vibes',
+      createdBy: 'John Doe'
+    },
+    {
+      name: 'Chill Vibes',
+      createdBy: 'John Doe'
+    },
+    {
+      name: 'Chill Vibes',
+      createdBy: 'John Doe'
+    }
+  ]
+  playlists: any[] = [
+    {
+      name: 'Chill Vibes',
+      createdBy: 'John Doe',
+    },
+    {
+      name: 'Chill Vibes',
+      createdBy: 'John Doe',
+    },
+    {
+      name: 'Chill Vibes',
+      createdBy: 'John Doe',
+    },
+    {
+      name: 'Chill Vibes',
+      createdBy: 'John Doe',
+    },
+    {
+      name: 'Chill Vibes',
+      createdBy: 'John Doe',
+    },
+    {
+      name: 'Chill Vibes',
+      createdBy: 'John Doe',
+    }
+  ];
 }

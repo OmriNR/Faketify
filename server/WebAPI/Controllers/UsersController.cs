84a3e8df7571e95f801abb946f9622233f315ac7
OmriNR <omrinir12@gmail.com>
2025-08-27 13:34:26 +0300

    Changing server from rust to C# and adding webAPI

---
+++ server/WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    [HttpGet]
+    [Produces("application/json")]
+    public IActionResult Get()
+    {
+        return Ok("Hello World");
+    }
+}
--- server/src/models/mod.rs
+++
@@ -1,7 +0,0 @@
-mod song_model;
-mod user_model;
-mod playlist_model;
-
-pub use song_model::*;
-pub use user_model::*;
-pub use playlist_model::*;
